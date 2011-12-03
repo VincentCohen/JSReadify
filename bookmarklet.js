136db16b6756e2293f09f39fa23d688b52942a5c
Vincent Cohen <v.cohen90@gmail.com>
2011-12-03 19:12:02 +0100

    Added new files and initial setup

---
+++ bookmarklet.js
@@ -0,0 +1,10 @@
+/**
+* JSReadify
+*	a better reading experience
+* TODO & NOTES:
+	http://benalman.com/code/test/jquery-run-code-bookmarklet/
+*/
+javascript:(function(){
+	var scriptTag = document.createElement('script');
+	scriptTag.setAttribute('src','http://vincentcohen.nl/');
+});
