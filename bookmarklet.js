12ef1d737884bdaafc6cd66749ea6ec94c5de790
Vincent Cohen <v.cohen90@gmail.com>
2011-12-03 19:25:33 +0100

    changes n tests

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,3 @@
-/**
-* JSReadify
-*	a better reading experience
-* TODO & NOTES:
-	http://benalman.com/code/test/jquery-run-code-bookmarklet/
-*/
 javascript:(function(){
-	var scriptTag = document.createElement('script');
-	scriptTag.setAttribute('src','https://raw.github.com/VincentCohen/JSReadify/master/readify.js');
-});
+	alert('hi from bookmarklet.js');
+})();  
