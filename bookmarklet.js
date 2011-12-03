14342862a3ed06459dbd1fe9d4d54f33cc180168
Vincent Cohen <v.cohen90@gmail.com>
2011-12-03 19:49:37 +0100

    goddamn changes

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,15 @@
-javascript:(function(){
-	alert('hi from me');
-})();  
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too  
+	script		= document.createElement( 'script' );  
+	script.src	= 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';  
+	script.onload=Readify;  
+	document.body.appendChild(script);  
+}  
+else {  
+    Readify();  
+}  
+  
+function Readify() {  
+    // The Kraken has been released, master!  
+    // Yes, I'm being childish. Place your code here  
+    alert('lets make this page beautifull!');
+}  
